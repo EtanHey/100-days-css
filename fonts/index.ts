6a3a6e5f34f5c95959597443d3cc04a41191b2a5
Etan Joseph Heyman <etan@heyman.net>
2023-03-26 20:07:45 -0500

    Structure change

--- fonts/index.ts
+++ fonts/index.ts
@@ -1,11 +1,11 @@
 import localFont from "next/font/local";
 
 const courierNew = localFont({
-  src: "../public/fonts/courier_new.ttf",
+  src: "../public/fonts/day-1/courier_new.ttf",
   variable: "--font-courier-new",
 });
 const courierNewBold = localFont({
-  src: "../public/fonts/courier_new_bold.ttf",
+  src: "../public/fonts/day-1/courier_new_bold.ttf",
   variable: "--font-courier-new-bold",
 });
 
