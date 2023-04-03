eb33b469bb3221199e62e5b9d403a5905fc7e093
Etan Joseph Heyman <etan@heyman.net>
2023-04-03 18:14:38 -0500

    Add Day 45 to Header

--- app/Header.tsx
+++ app/Header.tsx
@@ -41,6 +41,11 @@ const Header = () => {
               Day 44
             </Link>
           </li>
+          <li>
+            <Link href="/day-45" className="px-3 py-1 text-sm uppercase">
+              Day 45
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
--- app/day-44/README.md
+++ app/day-44/README.md
@@ -1,4 +1,1 @@
 # Things I learned:
- - A work around for random positioning with react maps and tailwind.
- - A work around for random seconds on animations using tailwind.
- - Working with NextJs 13.2 Image with custom width (fill).
