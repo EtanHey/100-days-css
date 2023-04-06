2509129240cf57be113fbc16c32c95817af109d1
Etan Joseph Heyman <etan@heyman.net>
2023-04-06 08:50:18 -0500

    Day 48: Done.

---
+++ components/day-48/CubeSide.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CubeSide = ({ className }: { className: string }) => {
+  return <div className={`absolute h-50 w-50 ${className}`}></div>;
+};
+
+export default CubeSide;
--- components/day-48/TotalDay48.tsx
+++ components/day-48/TotalDay48.tsx
@@ -1,12 +1,27 @@
 "use client";
 import AbsoluteFrame from "../AbsoluteFrame";
+import CubeSide from "./CubeSide";
 
 const TotalDay48 = () => {
   return (
-    <AbsoluteFrame
-      className={`rounded-md bg-[#E47512] shadow-fortyThreeFrame `}
-    >
-      <div>cube</div>
+    <AbsoluteFrame className={`Day48Frame`}>
+      <div className="Day48center">
+        {/* cube */}
+        <div className="cube">
+          {/* top */}
+          <CubeSide className={"top"} />
+          {/* bottom */}
+          <CubeSide className={"bottom"} />
+          {/* left */}
+          <CubeSide className={"left"} />
+          {/* right */}
+          <CubeSide className={"right"} />
+          {/* front */}
+          <CubeSide className={"front"} />
+          {/* back */}
+          <CubeSide className={"back"} />
+        </div>
+      </div>
     </AbsoluteFrame>
   );
 };
